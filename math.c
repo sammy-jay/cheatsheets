2df4a7907be67f40a884bcf737944a9d830e568f
Tolu Soneye <68499075+sammy-jay@users.noreply.github.com>
2022-07-05 02:44:52 +0100

    Create math.c

---
+++ math.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    
+    printf("Square root: %f\n", sqrt(16));
+    printf("Ceil number: %f\n", ceil(1.4));
+    printf("Floor number: %f\n", floor(1.4));
+    printf("Power: %f\n", pow(4, 3));
+    
+    return 0;
+}
