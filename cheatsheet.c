750a4a7d85a9f35ba653d83e980b5330c8f350a3
Tolu Soneye <68499075+sammy-jay@users.noreply.github.com>
2022-07-05 02:07:22 +0100

    Create cheatsheet.c

---
+++ cheatsheet.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+
+int main(){
+    
+    // // type variable_name = value;
+    // const int myAge = 23.6;
+    // double cgpa = 34.4737373664;
+    // // myNum = 24;
+    // char myName[10] = "Tolu";
+    
+    // char myChar1 = 's';
+    
+    // printf("Float: %lf\n", cgpa);
+    // printf("Char: %c\n", myChar1);
+    // printf("My name is %s and I'm %d years\n", myName, myAge);
+    // printf("I am learning the C language\n");
+    
+    // int myInt;
+    // float myFloat;
+    // double myDouble;
+    // char myChar;
+    
+    // printf("%lu\n", sizeof(myInt));
+    // printf("%lu\n", sizeof(myFloat));
+    // printf("%lu\n", sizeof(myDouble));
+    // printf("%lu\n", sizeof(myChar));
+    
+    // int x = 12, y = 10;
+    // if (x < y) printf("%d is less than %d", x, y);
+    // else if(x > y) printf("%d is greater than %d", x, y);
+    // else printf("%d and %d are equal", x, y);
+    
+    
+    // int val = 1;
+    
+    // switch(val){
+    //     case 1:
+    //         printf("apple is green");
+    //         break;
+    //     case 2:
+    //         printf("apple is red");
+    //         break;
+    //     default:
+    //         printf("no color");
+    //         break;
+    // }
+    
+    // int i = 0;
+    // while (i < 10){
+    //     printf("%d...\n", i);
+    //     i ++;
+    // }
+    
+    // i = 0;
+    
+    // do {
+    //     printf("%d...\n", i);
+    // } while(i < 0);
+    
+    
+    // for(int i=0; i < 10; i++){
+    //     printf("%d...\n", i);
+    // }
+    
+    // int myNumbers[5] = {1, 2, 3, 4, 5};
+    
+    // sizeof()
+    
+    
+    // char name[] = "Tolulope Soneye";
+    // char newName[] = {'T', 'o', 'l', '\0'};
+    // myNumbers[100] = 23;
+    // printf("%c", newName[33]);
+    
+    // int randNum;
+    // printf("Random number is: %p\n", &randNum);
+    // printf("Enter random number: ");
+    // scanf("%d", &randNum);
+    // printf("Random number is: %d\n\n", randNum);
+    
+    // char myName[10];
+    // const int secretNumber = 2;
+    // int guess;
+    // int guessCount = 1;
+    
+    // printf("Welcome to my guessing game\n\n");
+    // printf("Type a your name to continue: ");
+    
+    // scanf("%s", myName);
+    
+    // printf("Hello, %s\n\n", myName);
+ 
+  
+    // while(guess != secretNumber && guessCount <= 3){
+    //     printf("Please pick a number between 0 and 10: ");
+    //     scanf("%d", &guess);
+
+        
+    //     if (guess < secretNumber) {
+    //         printf("Too low, guess higher\n");
+    //     } else if (guess > secretNumber) {
+    //         printf("Too high, guess lower\n");
+    //     } else {
+    //         printf("Congrats to you\n");
+    //         return 0;
+    //     }
+        
+    //     if (guessCount == 3) {
+    //         printf("Out of turns, please try again!\n");
+    //         return 0;
+    //     }
+    //     guessCount++;
+    // } 
+    
+    
+    // int t = 20;
+    // int* pt = &t;
+    
+    // printf("number is: %d\n", t);
+    // printf("number is: %d\n", *pt);
+    
+    // printf("address is: %p\n", &t);
+    // printf("address is: %p\n", pt);
+    
+    // printf("address of the address is: %p\n", &pt);
+    
+    
+    return 0;
+}
